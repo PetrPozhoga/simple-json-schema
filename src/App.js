c214f62cef98ad2bf987da19e2533131b675c5e6
PetrPozhoga <pet24489@gmail.com>
2020-01-31 13:06:36 +0200

    update app comppen, create react hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { asyncGetSchema } from "./requests"
 import Form from "./components/Form"
 import './App.scss'
 
-class App extends React.Component {
+function App() {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      isSend: false,
-      initialState: {}
-    }
-  }
+  const [ isSend, setIsSend ] = useState(false)
+  const [ title, setTitle ] = useState('')
+  const [ description, setDescription ] = useState('')
+  const [ required, setRequired ] = useState([])
+  const [ properties, setProperties ] = useState({})
+  const [ initialState, setInitialState ] = useState({})
 
-  changeFields(event) {
-    let currentProperty = JSON.parse(JSON.stringify(this.state.properties[ event.target.name ]))
-    currentProperty.value = this.state.properties[ event.target.name ].type === 'integer' ?
+  const changeFields = (event) => {
+    let currentProperty = JSON.parse(JSON.stringify(properties[ event.target.name ]))
+    currentProperty.value = properties[ event.target.name ].type === 'integer' ?
       event.target.value.replace(/\D/, '') : event.target.type === 'checkbox' ? event.target.checked : event.target.value
-    this.setState({
-      properties: {
-        ...this.state.properties,
-        [ event.target.name ]: currentProperty
-      }
-    }, () => {
-      if (this.state.isSend) this.fieldValidation()
-    })
+    let propertiesCopy = { ...properties, [ event.target.name ]: currentProperty }
+    setProperties(propertiesCopy)
+    if (isSend) fieldValidation(propertiesCopy)
   }
 
-  async getSchema() {
+  const getSchema = async () => {
     try {
       let res = await asyncGetSchema()
       if (res.status !== 200 || !res.data) return false
@@ -35,85 +29,82 @@ class App extends React.Component {
         let { title, description, required, properties } = res.data
         Object.keys(properties).forEach(key => properties[ key ].value = properties[ key ].value || '')
         let initialState = { title, description, required, properties }
-        this.setState({ ...initialState, initialState })
+        setInitialState(initialState)
+        setTitle(title)
+        setDescription(description)
+        setRequired(required)
+        setProperties(properties)
       }
     } catch (err) {
       console.log(err, 'getSchema')
     }
   }
 
-  checkRequiredField() {
-    let properties = JSON.parse(JSON.stringify(this.state.properties))
-    this.state.required.forEach(propertyName => {
-      let currentProperty = properties[ propertyName ]
+  const checkRequiredField = (propertiesCopy) => {
+    required.forEach(propertyName => {
+      let currentProperty = propertiesCopy[ propertyName ]
       let errorMessage = 'Field ' + currentProperty.title + ' is required'
       if (currentProperty.value && (currentProperty.value.length || typeof currentProperty.value === "boolean"))
-        properties[ propertyName ] = {
+        propertiesCopy[ propertyName ] = {
           ...currentProperty,
           errorMessage: currentProperty.errorMessage === errorMessage ? '' : currentProperty.errorMessage
         }
 
       else {
-        console.log(errorMessage)
-        properties[ propertyName ] = { ...currentProperty, errorMessage }
+        propertiesCopy[ propertyName ] = { ...currentProperty, errorMessage }
       }
     })
-    this.setState({ properties }, () => this.checkMinLength())
+    setProperties(propertiesCopy)
+    return checkMinLength(propertiesCopy)
   }
 
-  checkMinLength() {
-    let properties = JSON.parse(JSON.stringify(this.state.properties))
-    Object.keys(properties).forEach(propertyName => {
-      let currentProperty = properties[ propertyName ]
+  const checkMinLength = (propertiesCopy) => {
+    Object.keys(propertiesCopy).forEach(propertyName => {
+      let currentProperty = propertiesCopy[ propertyName ]
       let errorMessage = 'The ' + currentProperty.title + ' field must be at least ' + currentProperty.minLength + ' characters'
 
       if (currentProperty.minLength) {
-        if (currentProperty.value && currentProperty.value.length >= currentProperty.minLength) properties[ propertyName ] = {
+        if (currentProperty.value && currentProperty.value.length >= currentProperty.minLength) propertiesCopy[ propertyName ] = {
           ...currentProperty,
           errorMessage: ''
         }
-        else properties[ propertyName ] = { ...currentProperty, errorMessage }
+        else propertiesCopy[ propertyName ] = { ...currentProperty, errorMessage }
       }
     })
-    this.setState({ properties })
+    setProperties(propertiesCopy)
+    return propertiesCopy
   }
 
-  fieldValidation() {
-    this.checkRequiredField()
+  const fieldValidation = (properties) => {
+    let propertiesCopy = checkRequiredField(properties)
+    return !Object.keys(propertiesCopy).some(item => !!propertiesCopy[item].errorMessage)
   }
 
-  sendForm(e) {
+  const sendForm = (e) => {
     e.preventDefault()
-    this.setState({
-      isSend: true
-    }, async () => {
-      await this.fieldValidation()
-      const { properties, initialState } = this.state
-      if (Object.keys(properties).every(propertyName => !!properties[ propertyName ].errorMessage === false)) {
-        console.log(this.state.properties)
-        this.setState({ properties: initialState.properties, isSend: false })
-      }
-    })
+    setIsSend(true)
+    let isValid = fieldValidation(JSON.parse(JSON.stringify(properties)))
+    if (isValid) {
+      setIsSend(false)
+      setProperties(initialState.properties)
+    }
   }
 
-  componentDidMount() {
-    this.getSchema();
-  }
+  useEffect(() => {
+    getSchema()
+  }, [])
 
-  render() {
-    const { title, description, properties, required } = this.state
-    return (
-      <div className="App">
-        <div className='container'>
-          <h1>{ title }</h1>
-          <h2>{ description }</h2>
-          { properties && required &&
-          <Form changeFields={ this.changeFields.bind(this) } sendForm={ this.sendForm.bind(this) }
-                required={ required } properties={ properties }/> }
-        </div>
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>{ title }</h1>
+        <h2>{ description }</h2>
+        { properties && required &&
+        <Form changeFields={ changeFields } sendForm={ sendForm }
+              required={ required } properties={ properties }/> }
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
